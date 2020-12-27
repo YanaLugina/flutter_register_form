183de018394e61501ed181dd374b907b1ff46059
YanaLugina <yana4961@gmail.com>
2020-12-28 00:03:56 +1000

    added fields and submit button in form

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -17,7 +17,52 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
         child: ListView(
           padding: EdgeInsets.all(16.0),
           children: <Widget>[
-
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Full Name *'
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Phone Number *'
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email address'
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Life Store'
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password *'
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Confirm Password *'
+              ),
+            ),
+            SizedBox(height: 15.0,),
+            RaisedButton(
+              onPressed: () {},
+              color: Colors.green,
+              child: Text(
+                'Submit Form',
+                style: TextStyle(
+                  color: Colors.white,
+                )
+              ),
+            ),
           ],
         ),
       ),
