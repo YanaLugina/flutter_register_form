842ab3eed6d0b1e82370a9034cb34b5f4a43425e
YanaLugina <yana4961@gmail.com>
2020-12-29 00:47:21 +1000

    added style for TextFields and TextFormFields

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -18,41 +18,88 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
           padding: EdgeInsets.all(16.0),
           children: <Widget>[
             TextField(
+              showCursor: true,
+              readOnly: true,
               decoration: InputDecoration(
-                labelText: 'Full Name *'
+                labelText: 'Full Name *',
+                hintText: 'What do people call you?',
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(
+                    Icons.delete_outline,
+                    color: Colors.red),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.lightBlue, width: 2.0)
+                ),
               ),
             ),
             SizedBox(height: 10.0,),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'Phone Number *'
+                labelText: 'Phone Number *',
+                hintText: 'Where can we reach you?',
+                helperText: 'Phone format: +7(XXX) XXX-XX-XX',
+                prefixIcon: Icon(Icons.call),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.lightBlue, width: 2.0)
+                ),
               ),
             ),
             SizedBox(height: 10.0,),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'Email address'
+                labelText: 'Email address',
+                hintText: 'Enter a email address',
+                icon: Icon(Icons.mail),
               ),
             ),
             SizedBox(height: 10.0,),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'Life Store'
+                labelText: 'Life Store',
+                hintText: 'Tell as about you self',
+                helperText: 'Keep it short, this is just a demo',
+                border: OutlineInputBorder(),
               ),
+              maxLines: 3,
             ),
             SizedBox(height: 20.0,),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'Password *'
+                labelText: 'Password *',
+                hintText: 'Enter the password',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility),
+                  onPressed: () {},
+                ),
+                icon: Icon(Icons.security),
               ),
             ),
             SizedBox(height: 10.0,),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'Confirm Password *'
+                labelText: 'Confirm Password *',
+                hintText: 'Confirm the password',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility),
+                  onPressed: () {},
+                ),
+                icon: Icon(Icons.border_color),
               ),
             ),
-            SizedBox(height: 15.0,),
+            SizedBox(height: 20.0,),
             RaisedButton(
               onPressed: () {},
               color: Colors.green,
